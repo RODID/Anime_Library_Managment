071f160b4ac8c8eb29619cdceef485f5e87bfde7
arjan <arjan@ARRU-HI>
2024-04-08 13:27:48 +0200

    update

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function Book(title, author, publisher, sbn, copies){
+// script.js
+
+
+function Manga(title, author, publisher, sbn, copies){
     this.title = title;
     this.author = author;
     this.publisher = publisher;
@@ -6,27 +9,50 @@ function Book(title, author, publisher, sbn, copies){
     this.copies = copies;
 }
 
+
+//This is a library array for the the added Mangas
 var library = [];
 
-function addBook(){
+/*Add function for the Mangas*/
+function addManga(){
     var title = document.getElementById("title").value;
     var author = document.getElementById("author").value;
     var publisher = document.getElementById("publisher").value;
     var sbn = document.getElementById("sbn").value;
     var copies = document.getElementById("copies").value;
-    var book = new Book(title, author, publisher, sbn, copies);
-    library.push(book);
-    displayBooks();
+    var manga = new Manga(title, author, publisher, sbn, copies);
+    library.push(manga);
+
+    //After pushing book display it in the library
+    displayManga();
+    //Updating the list of user manga.
+    updateMangaList();
  }
 
- function removeBook(){
-    var sbn = prompt("Enter the SBN of the book to remove: ");
+ /* Remove Function fot eh mangas in the list*/
+ function removeManga(){
+    var sbn = prompt("Enter the SBN of the Manga to remove: ");
     for (var i = 0; i < library.length; i++){
         if (library[i].sbn === sbn){
             library.splice(i, 1);
             break; 
         }
     }
-    displayBooks();
+    displayManga();
  }
- function
+
+ function displayManga(){
+    var mangaList = document.getElementById("mangaList");
+    mangaList.innerHTML = "";
+    for(var i = 0; i < library.length; i++){
+        var tr  = document.createElement("tr");
+        tr.innerHTML =`
+            <td>${library[i].title}</td>
+            <td>${library[i].author}</td>
+            <td>${library[i].publisher}</td>
+            <td>${library[i].sbn}</td>
+            <td>${library[i].copies}</td>
+        `;
+    mangaList.appendChild(tr);
+    }
+ }
